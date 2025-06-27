28c786a78a908bc36343eff4842b288f146a79a9
Adam <51168566+CodeCameleon@users.noreply.github.com>
2025-06-27 20:05:44 +0200

    Implemented the base entities for testing.

---
+++ CodeCameleon.EfCoreToolkit.Tests/Models/TestRegularEntity.cs
@@ -0,0 +1,22 @@
+﻿using CodeCameleon.EfCoreToolkit.BaseEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeCameleon.EfCoreToolkit.Tests.Models;
+
+/// <summary>
+/// A test implementation of <see cref="RegularEntity" /> for unit testing purposes.
+/// </summary>
+internal class TestRegularEntity
+    : RegularEntity
+{
+    /// <summary>
+    /// The unique identifier for the entity.
+    /// </summary>
+    [Key]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The name of the entity.
+    /// </summary>
+    public string? Name { get; set; }
+}
---
+++ CodeCameleon.EfCoreToolkit.Tests/Models/TestTemporaryEntity.cs
@@ -0,0 +1,22 @@
+﻿using CodeCameleon.EfCoreToolkit.BaseEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeCameleon.EfCoreToolkit.Tests.Models;
+
+/// <summary>
+/// A test implementation of <see cref="TemporaryEntity" /> for unit testing purposes.
+/// </summary>
+internal class TestTemporaryEntity
+    : TemporaryEntity
+{
+    /// <summary>
+    /// The unique identifier for the entity.
+    /// </summary>
+    [Key]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The name of the entity.
+    /// </summary>
+    public string? Name { get; set; }
+}
